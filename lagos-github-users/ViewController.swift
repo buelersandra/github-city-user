15f90f2fffcdea08c6014b81c5decd1c326ea52f
Beulah Ana <buelersandra@gmail.com>
2022-02-09 13:29:22 +0300

    setup

---
+++ lagos-github-users/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lagos-github-users
+//
+//  Created by Beulah Ana on 09/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
